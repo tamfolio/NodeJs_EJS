d9287b08da8e701da2aad19461db0dd6a2f76d0a
Tamilore <tamiloreolaniyi@gmail.com>
2023-02-01 06:31:59 +0100

    ejs

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.get('/', function(req,res){
+    var today = new Date();
+
+    if(today.getDay() === 6 || today.getDay === 0){
+        res.sendFile(__dirname + '/weekend.html')
+    } else {
+        res.sendFile(__dirname + '/weekday.html')
+    }
+});
+
+
+app.listen(3002, function(){
+    console.log("server up and running")
+})
