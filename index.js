0269afccbe80171f71d52972d65c9146b99c6b74
Tamilore <tamiloreolaniyi@gmail.com>
2023-02-02 12:20:37 +0530

    to-do

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var items = []
 
 app.get('/', function(req,res){
     var today = new Date();
@@ -19,8 +22,15 @@ app.get('/', function(req,res){
 
 
 
-    res.render("list", {kindOfDay: day})
+    res.render("list", {kindOfDay: day, newListItems: items})
 });
+
+app.post('/', function(req,res){
+    var item = req.body.newItem
+    
+    items.push(item)
+    res.redirect('/')
+})
 
 
 app.listen(3002, function(){
